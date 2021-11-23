e421a34e97ef818644b733bbe1cf47430e85041a
Antioxidant233 <80783484+Antioxidant233@users.noreply.github.com>
2021-11-23 18:18:42 -0500

    Update TableReader.java

--- src/Java/Data/TableReader.java
+++ src/Java/Data/TableReader.java
@@ -1,4 +1,4 @@
-package Data;
+package Java.Data;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,14 +9,32 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class TableReader {
+public class TableReader{
     Map<String ,String> user_info_map;
 
     public TableReader(){
         user_info_map = new HashMap<String,String>();
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("src/data/UserInfoTable.csv")); // filename here
+            BufferedReader reader = new BufferedReader(new FileReader("src/Java/data/UserInfoTable.csv")); // filename here
+            reader.readLine(); //Ignore line 1
+            String line;
+            while((line=reader.readLine())!=null){
+                String[] item = line.split(","); //split the items by comma
+
+                user_info_map.put(item[0], item[1]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Polymorphism on Constructor for testing methods without affecting the UerInfoTable.csv
+    public TableReader(String filename){
+        user_info_map = new HashMap<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename)); // filename here
             reader.readLine(); //Ignore line 1
             String line = null;
             while((line=reader.readLine())!=null){
@@ -48,9 +66,22 @@ public class TableReader {
         }
     }
 
+    //Overloading this method with a specified testing TableReader for testing.
+    public boolean AddNewUser(String user, String password, TableReader tr){
+        boolean duplicated = tr.user_info_map.containsKey(user);
+        if (!duplicated) {
+            user_info_map.put(user, password);
+            UpdateTable(user, password);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
     public void UpdateTable(String user, String password){
         try {
-            File csv = new File("src/data/UserInfoTable.csv");
+            File csv = new File("src/Java/data/UserInfoTable.csv");
 
             BufferedWriter bw = new BufferedWriter(new FileWriter(csv, true));
             bw.newLine();
@@ -61,6 +92,22 @@ public class TableReader {
             e.printStackTrace();
         }
     }
+//Overloading UpdateTable method just for testing.
+    public void UpdateTable(String user, String password, String filename){
+        try {
+            File csv = new File(filename);
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(csv, true));
+            bw.newLine();
+            bw.write(user + "," + password);
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("The TestOnly.csv has been updated.");
+    }
+
 
     public static void main(String[] args) {
         // Simple testing
