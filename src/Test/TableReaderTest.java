424d334f73a6b0b0920847faed662f44b1ec5cfa
Antioxidant233 <80783484+Antioxidant233@users.noreply.github.com>
2021-11-23 18:22:10 -0500

    Test for AddNewUser still affects the source data
    We want it just to make changes to the TestOnly.csv, but right now it would still affect the UserInfoTable.csv.

--- src/Test/TableReaderTest.java
+++ src/Test/TableReaderTest.java
@@ -25,7 +25,7 @@ class TableReaderTest {
     }
 
     @org.junit.jupiter.api.Test
-    void testAddNewUser() {
+    void testAddNewUser() { //Need to be fixed.
         TableReader info = new TableReader("src/Java/Data/TestOnly.csv");
         //When the username already exists in the csv file.
         assertFalse(info.AddNewUser("zhan666", "zhan999", info));
